b6d58ac06c8d572e9acf773a1ec19713fae862aa
TangWan <tangjingfan@gmail.com>
2024-12-30 15:36:57 +0800

    feat: init pygame structure

---
+++ src/standard_test.py
@@ -0,0 +1,17 @@
+import pygame
+import random
+import time
+
+# init pygame structure
+pygame.init()
+
+# size of the window
+screen_height, screen_width = 600, 800
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# define color set
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# quit pygame structure
+pygame.quit()
